2b01fb08153ca511a7c2875d6372c2ed6eb69617
Joel Granados <j.granados@samsung.com>
2023-10-13 14:54:19 +0200

    hw/nvme: Replace calls to dma-helpers with iov
    In order to remove the duality between dma-helpers and iov inside the
    nvme device we migrate to just using Qemu's iov infrastructure. This is
    achieved by removing the sg->qsg initialization from nvme_sg_init and
    making sure to replace dma-helpers with iov when in nvme_map_addr. The
    new nvme_map_addr_dma function will be called from nvme_map_addr for the
    non-pmr and non-cmd cases.
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>

--- hw/nvme/ctrl.c
+++ hw/nvme/ctrl.c
@@ -709,13 +709,7 @@ static inline void nvme_sg_init_header(NvmeCtrl *n, NvmeSg *sg, DMADirection dir
 
 static inline void nvme_sg_init(NvmeCtrl *n, NvmeSg *sg, bool dma)
 {
-    if (dma) {
-        pci_dma_sglist_init(&sg->qsg, PCI_DEVICE(n), 0);
-        sg->flags = NVME_SG_DMA;
-    } else {
-        qemu_iovec_init(&sg->iov, 0);
-    }
-
+    qemu_iovec_init(&sg->iov, 0);
     sg->flags |= NVME_SG_ALLOC;
 }
 
@@ -819,10 +813,30 @@ static uint16_t nvme_map_addr_pmr(NvmeCtrl *n, QEMUIOVector *iov, hwaddr addr,
     return NVME_SUCCESS;
 }
 
+
+static uint16_t nvme_map_addr_dma(NvmeCtrl *n, QEMUIOVector *iov, DMADirection dir,
+                                  hwaddr addr, size_t len)
+{
+    void *mem;
+    dma_addr_t dma_len;
+    AddressSpace *as = pci_get_address_space(&n->parent_obj);
+
+    while (len > 0) {
+        dma_len = len;
+        mem = dma_memory_map(as, addr, &dma_len, dir, MEMTXATTRS_UNSPECIFIED);
+
+        if (!mem)
+            return NVME_INVALID_SGL_SEG_DESCR;
+
+        qemu_iovec_add(iov, mem, dma_len);
+        addr += dma_len;
+        len -= dma_len;
+    }
+    return NVME_SUCCESS;
+}
+
 static uint16_t nvme_map_addr(NvmeCtrl *n, NvmeSg *sg, hwaddr addr, size_t len)
 {
-    bool cmb = false, pmr = false;
-
     if (!len) {
         return NVME_SUCCESS;
     }
@@ -833,39 +847,17 @@ static uint16_t nvme_map_addr(NvmeCtrl *n, NvmeSg *sg, hwaddr addr, size_t len)
         return NVME_DATA_TRAS_ERROR;
     }
 
-    if (nvme_addr_is_cmb(n, addr)) {
-        cmb = true;
-    } else if (nvme_addr_is_pmr(n, addr)) {
-        pmr = true;
-    }
-
-    if (cmb || pmr) {
-        if (sg->flags & NVME_SG_DMA) {
-            return NVME_INVALID_USE_OF_CMB | NVME_DNR;
-        }
-
-        if (sg->iov.niov + 1 > IOV_MAX) {
-            goto max_mappings_exceeded;
-        }
-
-        if (cmb) {
-            return nvme_map_addr_cmb(n, &sg->iov, addr, len);
-        } else {
-            return nvme_map_addr_pmr(n, &sg->iov, addr, len);
-        }
-    }
-
-    if (!(sg->flags & NVME_SG_DMA)) {
-        return NVME_INVALID_USE_OF_CMB | NVME_DNR;
-    }
-
-    if (sg->qsg.nsg + 1 > IOV_MAX) {
+    if (sg->iov.niov + 1 > IOV_MAX) {
         goto max_mappings_exceeded;
     }
 
-    qemu_sglist_add(&sg->qsg, addr, len);
-
-    return NVME_SUCCESS;
+    if (nvme_addr_is_cmb(n, addr)) {
+        return nvme_map_addr_cmb(n, &sg->iov, addr, len);
+    } else if (nvme_addr_is_pmr(n, addr)) {
+        return nvme_map_addr_pmr(n, &sg->iov, addr, len);
+    } else {
+        return nvme_map_addr_dma(n, &sg->iov, sg->dir, addr, len);
+    }
 
 max_mappings_exceeded:
     NVME_GUEST_ERR(pci_nvme_ub_too_many_mappings,
