9f0e49445beff08636753a7f8f33d57ce3e94819
Joel Granados <j.granados@samsung.com>
2023-10-17 11:21:25 +0200

    hw/nvme: Remove DMA subsystem duality from nvme_sg_unmap
    There is no longer need to differentiate between iovec and DMA memory
    mappings within the nvme device. This duality is removed from
    nvme_sg_unmap.
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>

--- hw/nvme/ctrl.c
+++ hw/nvme/ctrl.c
@@ -718,12 +718,7 @@ static inline void nvme_sg_unmap(NvmeSg *sg)
         return;
     }
 
-    if (sg->flags & NVME_SG_DMA) {
-        qemu_sglist_destroy(&sg->qsg);
-    } else {
-        qemu_iovec_destroy(&sg->iov);
-    }
-
+    qemu_iovec_destroy(&sg->iov);
     memset(sg, 0x0, sizeof(*sg));
 }
 
