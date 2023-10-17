280394b0942b26622e1ed27964bce2457a380368
Joel Granados <j.granados@samsung.com>
2023-10-17 11:17:02 +0200

    hw/nvme: Remove DMA subsystem duality from nvme_tx
    There is no longer need to differentiate between iovec and DMA memory
    mappings within the nvme device. In this commit this duality is removed
    from nvme_tx
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>

--- hw/nvme/ctrl.c
+++ hw/nvme/ctrl.c
@@ -1324,33 +1324,17 @@ static uint16_t nvme_tx(NvmeCtrl *n, NvmeSg *sg, void *ptr, uint32_t len,
 {
     assert(sg->flags & NVME_SG_ALLOC);
 
-    if (sg->flags & NVME_SG_DMA) {
-        const MemTxAttrs attrs = MEMTXATTRS_UNSPECIFIED;
-        dma_addr_t residual;
+    size_t bytes;
 
-        if (dir == NVME_TX_DIRECTION_TO_DEVICE) {
-            dma_buf_write(ptr, len, &residual, &sg->qsg, attrs);
-        } else {
-            dma_buf_read(ptr, len, &residual, &sg->qsg, attrs);
-        }
-
-        if (unlikely(residual)) {
-            trace_pci_nvme_err_invalid_dma();
-            return NVME_INVALID_FIELD | NVME_DNR;
-        }
+    if (dir == NVME_TX_DIRECTION_TO_DEVICE) {
+        bytes = qemu_iovec_to_buf(&sg->iov, 0, ptr, len);
     } else {
-        size_t bytes;
+        bytes = qemu_iovec_from_buf(&sg->iov, 0, ptr, len);
+    }
 
-        if (dir == NVME_TX_DIRECTION_TO_DEVICE) {
-            bytes = qemu_iovec_to_buf(&sg->iov, 0, ptr, len);
-        } else {
-            bytes = qemu_iovec_from_buf(&sg->iov, 0, ptr, len);
-        }
-
-        if (unlikely(bytes != len)) {
-            trace_pci_nvme_err_invalid_dma();
-            return NVME_INVALID_FIELD | NVME_DNR;
-        }
+    if (unlikely(bytes != len)) {
+        trace_pci_nvme_err_invalid_dma();
+        return NVME_INVALID_FIELD | NVME_DNR;
     }
 
     return NVME_SUCCESS;
